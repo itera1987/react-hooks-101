60c60f4065a7d765bce00f7fbc269d5bd8a7d9ab
itera1987 <46280873+itera1987@users.noreply.github.com>
2020-08-02 19:52:18 +0900

    my first React Hooks App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const defaultValue = 0
+  const [count, setCount] = useState(defaultValue)
+
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+  const double = () => setCount(count * 2)
+  const reset = () => setCount(defaultValue)
+
+  const divide3 = () => {
+    setCount(count % 3 === 0 ? count / 3 : count)
+  }
+
   return (
-    <div>
-      This is React template
-    </div>
-  );
+    <>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={double}>*2</button>
+        <button onClick={divide3}>3で割る</button>
+      </div>
+      <div>
+        <button onClick={reset}>reset</button>
+      </div>
+    </>
+  )
 }
 
 export default App;
