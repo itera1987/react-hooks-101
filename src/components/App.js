79ac84e2a74c5d5ff09dc8f1402f7939706a6e5d
itera1987 <46280873+itera1987@users.noreply.github.com>
2020-08-09 20:13:46 +0900

    use context

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { useReducer } from 'react';
 
 import EventForm from "./EventForm"
 import Events from "./Events";
-
+import AppContext from "../contexts/AppContext";
 import reducer from "../reducers";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -11,10 +11,12 @@ const App = () => {
   const [state, dispatch] = useReducer(reducer, [])
 
   return (
-    <div className="container-fluid">
-      <EventForm state={state} dispatch={dispatch} />
-      <Events state={state} dispatch={dispatch} />      
-    </div>
+    <AppContext.Provider value={ "Fuck you" }>
+      <div className="container-fluid">
+        <EventForm state={state} dispatch={dispatch} />
+        <Events state={state} dispatch={dispatch} />      
+      </div>
+    </AppContext.Provider>
   );
 }
 
