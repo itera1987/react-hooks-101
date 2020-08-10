50ee520b3642e976958e71ee8dfeb9dcdb34a502
itera1987 <46280873+itera1987@users.noreply.github.com>
2020-08-10 09:47:41 +0900

    refactor events reducer

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,11 @@ import reducer from "../reducers";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, [])
+  const initialState = {
+    events: [],
+  }
+  
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   return (
     <AppContext.Provider value={{ state, dispatch }}>
